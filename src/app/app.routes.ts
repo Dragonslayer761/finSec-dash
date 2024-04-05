70f1fc4c32fc3a822a000bed300c1792a6b9da7d
Subham Chowdhur <sc761851@gmail.com>
2024-04-05 20:58:44 +0530

    routes and new policy and reports component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,30 @@ import { authGuard } from './Gaurd/auth.guard';
 import { DashboardComponent } from './Component/dashboard/dashboard.component';
 import { SignUpComponent } from './Component/sign-up/sign-up.component';
 import { ForgetPasswordComponent } from './Component/forget-password/forget-password.component';
-
+import { PolicyComponent } from './Component/policy/policy.component';
+import { ReportsComponent } from './Component/reports/reports.component';
 
 export const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
-    children: [{ path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] }],
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+        canActivate: [authGuard],
+      },
+      {
+        path: 'policy',
+        component: PolicyComponent,
+        canActivate: [authGuard]
+      },
+      {
+        path: 'reports',
+        component: ReportsComponent,
+        canActivate: [authGuard],
+      },
+    ],
     canActivate: [authGuard],
   },
   { path: 'login', component: LoginComponent },
