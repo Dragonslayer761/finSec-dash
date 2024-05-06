90e28f9002fc53f94ac3f4bc8815cbf8a58d2eba
Subham Chowdhur <sc761851@gmail.com>
2024-05-07 00:18:01 +0530

    add customer dilog added

--- src/app/Component/customer/customer.component.ts
+++ src/app/Component/customer/customer.component.ts
@@ -1,23 +1,43 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
 import { Customer } from '../../Interface/interface.model';
 import { CommonModule } from '@angular/common';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import {
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogTitle,
+} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-customer',
   standalone: true,
-  imports: [CommonModule, MatSortModule, MatTableModule,MatButtonModule],
+  imports: [
+    CommonModule,
+    MatSortModule,
+    MatTableModule,
+    MatButtonModule,
+    MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogTitle,
+    MatIcon,
+  ],
   templateUrl: './customer.component.html',
   styleUrl: './customer.component.scss',
 })
 export class CustomerComponent {
   @ViewChild(MatSort) sort: MatSort;
+  @ViewChild('testDilog', { static: true }) testDilog:TemplateRef<any>;
 
   customerList: Customer[];
   displayHeader: string[];
   dataSource: MatTableDataSource<Customer>;
+  constructor(public dilog: MatDialog) {}
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
@@ -43,5 +63,8 @@ export class CustomerComponent {
     //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
     //Add 'implements AfterViewInit' to the class.
     this.dataSource.sort = this.sort;
+  }
+  addCustomer() {
+    this.dilog.open(this.testDilog);
   }
 }
